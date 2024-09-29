f237d100b18cfd428a0a989383da016521b936cc
Kennedy Kimathi <jrkennedy027@gmail.com>
2024-09-29 18:12:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle the menu visibility when the menu icon is clicked
+document.getElementById('menu-bars').addEventListener('click', function () {
+    const menu = document.getElementById('navMenu');
+    menu.classList.toggle('show-menu');
+});
